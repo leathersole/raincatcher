92e004b2b256112c833ff5375bcef8c60381bcf9
sebastienblanc <scm.blanc@gmail.com>
2015-10-01 14:15:56 +0200

    Add sync config properties

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports = {
-  apiHost: 'http://localhost:8080'
-, apiPath: '/api/wfm/workorder'
+  apiHost: 'http://localhost:8080',
+  apiPath: '/api/wfm/workorder',
+  datasetId : 'workOrdersList',
+  syncOptions : {
+    "sync_frequency" : 2,
+    "storage_strategy": "dom"
+  }
+
 }
