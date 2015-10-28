90121f345887f2b2433a22b1c572ba94eb22fa91
Brian Leathem <bleathem@gmail.com>
2015-10-28 07:22:43 -0700

    Wrapped the angular version of the mediator#request method in an angular promise

--- ng-module.js
+++ ng-module.js
@@ -4,7 +4,15 @@ var mediator = require('./mediator');
 
 angular.module('wfm.core.mediator', ['ng'])
 
-.factory('mediator', function mediatorService($log) {
+.factory('mediator', function mediatorService($q, $log) {
+  var originalRequest = mediator.request;
+
+  // monkey patch the request function, wrapping the returned promise as an angular promise
+  mediator.request = function() {
+    var promise = originalRequest.apply(mediator, arguments);
+    return $q.when(promise);
+  };
+
   return mediator;
 });
 
