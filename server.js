e0d1ea6b10a6066f3ac0698a75addd0ebd326a01
Brian Leathem <bleathem@gmail.com>
2015-11-02 15:16:22 -0800

    Changes to consume the wfm-sync module

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var _ = require('lodash');
-var sync = require('./lib/sync-server')
+var _ = require('lodash')
+  , sync = require('fh-wfm-sync/sync-server')
+  , config = require('./lib/config')
+
 
 module.exports = function(mediator, app, mbaasApi) {
-  sync.init(mediator, mbaasApi);
+  sync.init(mediator, mbaasApi, config.datasetId, config.syncOptions);
 };
