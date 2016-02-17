6437e66132686f09a81b734e816b42d87b3e4788
Brian Leathem <bleathem@gmail.com>
2016-02-16 19:11:42 -0800

    Added copyright headers to all the files

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,8 @@
+/**
+* CONFIDENTIAL
+* Copyright 2016 Red Hat, Inc. and/or its affiliates.
+* This is unpublished proprietary source code of Red Hat.
+**/
 require('./message-detail.tpl.html.js');
 require('./message-form.tpl.html.js');
 require('./message-list.tpl.html.js');
