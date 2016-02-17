c1421a32af9abd9355a207db72d2b6ce2923d231
Brian Leathem <bleathem@gmail.com>
2016-02-16 19:11:43 -0800

    Added copyright headers to all the files

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,8 @@
+/**
+* CONFIDENTIAL
+* Copyright 2016 Red Hat, Inc. and/or its affiliates.
+* This is unpublished proprietary source code of Red Hat.
+**/
 require('./workorder-form.tpl.html.js');
 require('./workorder-list.tpl.html.js');
 require('./workorder.tpl.html.js');
