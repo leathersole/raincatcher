7c6f5aaf0fe6932997b8a12cc25e7139a5cd1164
Brian Leathem <bleathem@gmail.com>
2016-02-16 19:11:42 -0800

    Added copyright headers to all the files

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
+/**
+* CONFIDENTIAL
+* Copyright 2016 Red Hat, Inc. and/or its affiliates.
+* This is unpublished proprietary source code of Red Hat.
+**/
 require('./signature-form.tpl.html.js');
 require('./signature.tpl.html.js');
