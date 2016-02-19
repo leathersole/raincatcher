6d3de7adc7c1f0dda140488549d711a660980553
Brian Leathem <bleathem@gmail.com>
2016-02-19 12:32:38 -0800

    Ported the map display code from the portal

---
+++ dist/workorder-map.tpl.html.js
@@ -0,0 +1,17 @@
+var ngModule;
+try {
+  ngModule = angular.module('wfm.workorder.directives');
+} catch (e) {
+  ngModule = angular.module('wfm.workorder.directives', []);
+}
+
+ngModule.run(['$templateCache', function ($templateCache) {
+  $templateCache.put('wfm-template/workorder-map.tpl.html',
+    '<!--\n' +
+    ' CONFIDENTIAL\n' +
+    ' Copyright 2016 Red Hat, Inc. and/or its affiliates.\n' +
+    ' This is unpublished proprietary source code of Red Hat.\n' +
+    '-->\n' +
+    '<div id=\'gmap_canvas\'></div>\n' +
+    '');
+}]);
