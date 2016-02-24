7dd593dfe464023009ff02b92d7c0fcaabeecd50
Andres Galante <agalante@gmail.com>
2016-02-24 16:16:02 -0300

    material-upgrade styles fix

--- dist/workorder.tpl.html.js
+++ dist/workorder.tpl.html.js
@@ -13,24 +13,25 @@ ngModule.run(['$templateCache', function ($templateCache) {
     ' This is unpublished proprietary source code of Red Hat.\n' +
     '-->\n' +
     '  <md-list>\n' +
-    '    <md-list-item class="md-2-line" >\n' +
+    '\n' +
+    '    <md-list-item class="md-2-line md-long-text">\n' +
     '      <md-icon md-font-set="material-icons">place</md-icon>\n' +
     '      <div class="md-list-item-text">\n' +
-    '        <h3>{{workorder.address}}</h3>\n' +
-    '        <h4>{{workorder.location[0]}}, {{workorder.location[1]}}</h4>\n' +
-    '        <p>Address</p>\n' +
+    '         <h3>{{workorder.location[0]}}, {{workorder.location[1]}}</h3>\n' +
+    '         <p>\n' +
+    '           {{workorder.address}}\n' +
+    '         </p>\n' +
     '      </div>\n' +
-    '    <md-divider></md-divider>\n' +
     '    </md-list-item>\n' +
-    '\n' +
+    '    <md-divider></md-divider>\n' +
     '    <md-list-item class="md-2-line" >\n' +
     '      <md-icon md-font-set="material-icons">assignment</md-icon>\n' +
     '      <div class="md-list-item-text">\n' +
     '        <h3>{{workorder.title}}</h3>\n' +
     '        <p>Workorder</p>\n' +
     '      </div>\n' +
-    '    <md-divider></md-divider>\n' +
     '    </md-list-item>\n' +
+    '    <md-divider></md-divider>\n' +
     '\n' +
     '    <md-list-item class="md-2-line" >\n' +
     '      <md-icon md-font-set="material-icons">event</md-icon>\n' +
@@ -38,8 +39,8 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '        <h3>{{workorder.finishTimestamp | date:\'yyyy-MM-dd\' }}</h3>\n' +
     '        <p>Finish Date</p>\n' +
     '      </div>\n' +
-    '    <md-divider></md-divider>\n' +
     '    </md-list-item>\n' +
+    '    <md-divider></md-divider>\n' +
     '\n' +
     '    <md-list-item class="md-2-line" >\n' +
     '      <md-icon md-font-set="material-icons">schedule</md-icon>\n' +
@@ -47,8 +48,8 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '        <h3>{{workorder.finishTimestamp | date:\'HH:mm:ss Z\' }}</h3>\n' +
     '        <p>Finish Time</p>\n' +
     '      </div>\n' +
-    '    <md-divider></md-divider>\n' +
     '    </md-list-item>\n' +
+    '    <md-divider></md-divider>\n' +
     '\n' +
     '    <md-list-item class="md-2-line" ng-show="assignee && assignee.name">\n' +
     '      <md-icon md-font-set="material-icons">person</md-icon>\n' +
@@ -56,8 +57,9 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '        <h3>{{assignee.name}}</h3>\n' +
     '        <p>Asignee</p>\n' +
     '      </div>\n' +
-    '    <md-divider></md-divider>\n' +
     '    </md-list-item>\n' +
+    '    <md-divider></md-divider>\n' +
+    '\n' +
     '  </md-list>\n' +
     '\n' +
     '  <md-subheader class="md-no-sticky">Work Summary</md-subheader>\n' +
