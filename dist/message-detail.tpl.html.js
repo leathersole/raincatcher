ecff795b640e0aaefbdc0019cb1f7b685be75b0e
sebastien blanc <sblanc@localhost.localdomain>
2016-03-16 19:18:44 +0100

    applied agalante scroll fix

--- dist/message-detail.tpl.html.js
+++ dist/message-detail.tpl.html.js
@@ -19,25 +19,26 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '    </h3>\n' +
     '  </div>\n' +
     '</md-toolbar>\n' +
+    '<div class="wfm-maincol-scroll">\n' +
+    '  <div class="message" layout-padding layout-margin>\n' +
     '\n' +
-    '<div class="message" layout-padding layout-margin>\n' +
+    '    <div class="message-header">\n' +
+    '      <div class="md-body-1">\n' +
+    '        <span>From:</span> {{ctrl.message.sender.name}}\n' +
+    '      </div>\n' +
+    '      <div class="md-body-1">\n' +
+    '        <span>To:</span> {{ctrl.message.receiver.name}}\n' +
+    '      </div>\n' +
+    '      <div class="md-body-1">\n' +
+    '        <span>Status:</span> {{ctrl.message.status}}\n' +
+    '      </div>\n' +
+    '      <!--<div class="md-body-1 time-stamp">11:38 AM (3 hours ago)</div>-->\n' +
+    '    </div>\n' +
     '\n' +
-    '  <div class="message-header">\n' +
-    '    <div class="md-body-1">\n' +
-    '      <span>From:</span> {{ctrl.message.sender.name}}\n' +
-    '    </div>\n' +
-    '    <div class="md-body-1">\n' +
-    '      <span>To:</span> {{ctrl.message.receiver.name}}\n' +
-    '    </div>\n' +
-    '    <div class="md-body-1">\n' +
-    '      <span>Status:</span> {{ctrl.message.status}}\n' +
-    '    </div>\n' +
-    '    <!--<div class="md-body-1 time-stamp">11:38 AM (3 hours ago)</div>-->\n' +
+    '    <md-divider></md-divider>\n' +
+    '\n' +
+    '    <p class="md-body-1">{{ctrl.message.content}}</p>\n' +
     '  </div>\n' +
-    '\n' +
-    '  <md-divider></md-divider>\n' +
-    '\n' +
-    '  <p class="md-body-1">{{ctrl.message.content}}</p>\n' +
     '</div>\n' +
     '');
 }]);
