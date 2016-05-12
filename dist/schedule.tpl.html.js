36ff87f19723f1aa746d0dd9a4b5fc4a2dc736e5
Brian Leathem <bleathem@gmail.com>
2016-05-12 09:31:10 -0700

    Corrected the case on the workorderFilter

--- dist/schedule.tpl.html.js
+++ dist/schedule.tpl.html.js
@@ -66,7 +66,7 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '        Workorders\n' +
     '      </h3>\n' +
     '    </div>\n' +
-    '    <span ng-repeat="workorder in ctrl.workorders | filter:workOrderfilter" class="wfm-chip wfm-chip-no-picture" draggable="true" data-workorderId="{{workorder.id}}">\n' +
+    '    <span ng-repeat="workorder in ctrl.workorders | filter:workorderFilter" class="wfm-chip wfm-chip-no-picture" draggable="true" data-workorderId="{{workorder.id}}">\n' +
     '      <schedule-workorder-chip workorder="workorder"></schedule-workorder-chip>\n' +
     '    </span>\n' +
     '  </div>\n' +
