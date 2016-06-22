d20d54de23da9c108439bcdd2cdc8fbed0a2050f
sebastien blanc <sblanc@localhost.localdomain>
2016-06-22 17:20:15 +0200

    removed license headers

--- dist/schedule.tpl.html.js
+++ dist/schedule.tpl.html.js
@@ -7,11 +7,6 @@ try {
 
 ngModule.run(['$templateCache', function ($templateCache) {
   $templateCache.put('wfm-template/schedule.tpl.html',
-    '<!--\n' +
-    'CONFIDENTIAL\n' +
-    'Copyright 2016 Red Hat, Inc. and/or its affiliates.\n' +
-    'This is unpublished proprietary source code of Red Hat.\n' +
-    '-->\n' +
     '<md-toolbar class="wfm-scheduler-toolbar">\n' +
     '  <div class="md-toolbar-tools">\n' +
     '    <h3>\n' +
