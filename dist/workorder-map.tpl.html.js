4f2ce7a3a3bd90b0dc2dc2d790aaffbc529d16cd
sebastien blanc <sblanc@localhost.localdomain>
2016-06-22 17:20:15 +0200

    removed license headers

--- dist/workorder-map.tpl.html.js
+++ dist/workorder-map.tpl.html.js
@@ -7,11 +7,6 @@ try {
 
 ngModule.run(['$templateCache', function ($templateCache) {
   $templateCache.put('wfm-template/workorder-map.tpl.html',
-    '<!--\n' +
-    ' CONFIDENTIAL\n' +
-    ' Copyright 2016 Red Hat, Inc. and/or its affiliates.\n' +
-    ' This is unpublished proprietary source code of Red Hat.\n' +
-    '-->\n' +
     '<div id=\'gmap_canvas\'></div>\n' +
     '');
 }]);
