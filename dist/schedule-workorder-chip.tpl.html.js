7047ddcb2b9f13e4be47b73b34d1d0703117ca64
sebastien blanc <sblanc@localhost.localdomain>
2016-06-23 16:25:05 +0200

    remove id from headers

--- dist/schedule-workorder-chip.tpl.html.js
+++ dist/schedule-workorder-chip.tpl.html.js
@@ -8,7 +8,7 @@ try {
 ngModule.run(['$templateCache', function ($templateCache) {
   $templateCache.put('wfm-template/schedule-workorder-chip.tpl.html',
     '<span class="wfm-chip wfm-chip-no-picture" style="width:300px;">\n' +
-    '  <span class="wfm-chip-name" >{{ctrl.workorder.type}} #{{ctrl.workorder.id}}</span>\n' +
+    '  <span class="wfm-chip-name" >{{ctrl.workorder.type}} - {{ctrl.workorder.title}}</span>\n' +
     '</span>\n' +
     '');
 }]);
--- dist/workorder-item.tpl.html.js
+++ dist/workorder-item.tpl.html.js
@@ -8,7 +8,7 @@ try {
 ngModule.run(['$templateCache', function ($templateCache) {
   $templateCache.put('wfm-template/workorder-item.tpl.html',
     '<span class="wfm-chip wfm-chip-no-picture" style="width:300px">\n' +
-    '  <span class="wfm-chip-name">{{workorder.type}} #{{workorder.id}}</span>\n' +
+    '  <span class="wfm-chip-name">{{workorder.type}} - {{workorder.title}}</span>\n' +
     '</span>\n' +
     '');
 }]);
